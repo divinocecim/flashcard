af1dbcf7900787a8ff485f70a76d208556745bc4
Divino Cecim da Silva <153013734+divinocecim@users.noreply.github.com>
2024-01-18 09:58:53 -0300

    Parte de login e tabelas relativas ao flashcard prontas

--- study_async/settings.py
+++ study_async/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
+    'flashcard',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,15 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Error Messages
+
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from usuarios.models import Categoria, Flashcard
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Flashcard)
